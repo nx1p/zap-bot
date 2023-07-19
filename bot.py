9b738baaf8f832acbb0a716536ff247e06e8438c
nx1p <email@marci.tech>
2023-07-19 16:22:04 +0800

    Refactored on_message logic into functions

--- bot.py
+++ bot.py
@@ -43,13 +43,20 @@ async def on_ready():
     await setup_edgegpt()
     print("Chatbot is ready")
 
+def is_thread(message: discord.Message) -> bool:
+    """Check if message is from a thread \n
+    - message: discord.Message
+    - returns True if the message is in a thread
+    - returns False otherwise"""
+    return isinstance(message.channel, discord.Thread)
+
 def thread_owner_is_me(message):
     """Check if message is from a thread owned by the bot. \n
     This function is used to check if the bot should reply to a message. 
     - message: discord.Message
     - returns True if the message is in a thread owned by the bot
     - returns False otherwise"""
-    if isinstance(message.channel, discord.Thread): 
+    if is_thread(message): 
         thread = message.channel
         if thread.owner == bot.user:
             return True
@@ -72,67 +79,77 @@ def should_i_reply(message):
         if bot.user.mentioned_in(message): # must contain a mention of the bot
             return True
 
+def cleanup_message(message):
+    # Check if the message content has a mention
+    if bot.user.mentioned_in(message):
+        # Get the message content without the mention
+        return message.clean_content.replace(bot.user.mention, "").strip()
+    else:
+        # If there is no mention, use the original message content
+        return message.clean_content.strip()
+
+async def ask_bchat(message_content):
+    """Send a message to the edgegpt chatbot and return the response.
+    - returns: Str"""
+    # The parameters for edgegpt chatbot
+    params = {
+        "conversation_style": ConversationStyle.creative,
+        "simplify_response": True
+    }
+
+    # Concatenate the prompt and the user's input
+    prompt = (f"{preprompt}"
+              f"{message_content}")
+
+    # Add the prompt to the params dictionary
+    params["prompt"] = prompt
+
+    # Send the request and get the response
+    # Use the asterisk operator to unpack the params dictionary
+    response = await b_chat.ask(**params) # Use await here
+
+    # Try to get the text from the response
+    try:
+        text = response["text"]
+    except KeyError:
+        # The response did not have a text key
+        return "Sorry, something went wrong with the chatbot. Please try again later."
+    return text
 
 # Event handler
 # - Runs when a message is received
 @bot.event
 async def on_message(message):
-    # Log received messages
-    print(f"Message from {message.author}: {message.content}")
-    # Check if the bot is mentioned or if the message is in a thread owned by the bot and the message is not from another bot
-    if should_i_reply(message):
-        # Check if the message content has a mention
-        if bot.user.mentioned_in(message):
-            # Get the message content without the mention
-            message_content = message.clean_content.replace(bot.user.mention, "").strip()
-        else:
-            # If there is no mention, use the original message content
-            message_content = message.clean_content.strip()
-        # The parameters for edgegpt chatbot
-        params = {
-            # Use one of the ConversationStyle values here
-            "conversation_style": ConversationStyle.creative,
-            "simplify_response": True
-        }
-        # Concatenate the prompt and the user's input
-        prompt = (f"{preprompt}"
-                  f"{message_content}")
+    print(f"Message from {message.author}: {message.content}") # Log received messages
 
-        # Add the prompt to the params dictionary
-        params["prompt"] = prompt
+    if should_i_reply(message): # Does this message meet the criteria for a reply?
+        message_content = cleanup_message(message) # Remove mention or leading/trailing whitespace
+        if is_thread(message): # Check if there is already a thread for this message or create one with name "Chat"
+            thread = message.channel
+        else:
+            thread = await message.create_thread(name="Chat")
         
-        # Create a typing context manager with async with
+        
+        
+        # Create a typing context manager
         async with message.channel.typing():
-            # Send the request and get the response
-            # Use the asterisk operator to unpack the params dictionary
-            response = await b_chat.ask(**params) # Use await here
+            text = await ask_bchat(message_content) # Send the message to the chatbot and get the response
         
-        # Try to get the text from the response
-        try:
-            text = response["text"]
-            # Check if there is already a thread for this message or create one with name "Chat"
-            # Use hasattr() to check if message.thread exists
-            # todo: fix this section (hasattr(message, "thread") is not the correct way)
-                # idk what i was thinking there lol 
-            thread = message.thread if hasattr(message, "thread") else await message.create_thread(name="Chat")
-            # Check if the text is longer than the limit
-            while len(text) > CHAR_LIMIT:
-                # Find the last newline before the limit
-                index = text.rfind("\n", 0, CHAR_LIMIT)
-                # If there is no newline, use the limit as index
-                if index == -1:
-                    index = CHAR_LIMIT
-                # Slice the text from 0 to index and store it in chunk
-                chunk = text[:index]
-                # Send the chunk as a reply in the thread
-                await thread.send(chunk)
-                # Update the text by slicing it from index to end
-                text = text[index:]
-            # Send the remaining text as a reply in the same thread
-            await thread.send(text)
-        except KeyError:
-            # The response did not have a text key
-            await message.channel.send("Sorry, something went wrong with the chatbot. Please try again later.")
+        # Check if the text is longer than the limit and chunk it if necessary
+        while len(text) > CHAR_LIMIT:
+            # Find the last newline before the limit
+            index = text.rfind("\n", 0, CHAR_LIMIT)
+            # If there is no newline, use the limit as index
+            if index == -1:
+                index = CHAR_LIMIT
+            # Slice the text from 0 to index and store it in chunk
+            chunk = text[:index]
+            # Send the chunk as a reply in the thread
+            await thread.send(chunk)
+            # Update the text by slicing it from index to end
+            text = text[index:]
+        # Send the remaining text as a reply in the same thread
+        await thread.send(text)
 
 if __name__ == "__main__": # If script run
     bot.run(get_token('./secrets/config.json')) # Run bot with token from config file
