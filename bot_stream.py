51365ec1e66cd30cc3fb183bf8119db366376c6a
nx1p <email@marci.tech>
2023-08-11 18:36:10 +0800

    wip version with streaming

---
+++ bot_stream.py
@@ -0,0 +1,236 @@
+# Import the libraries
+import discord
+from discord.ext import commands
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
+import asyncio, json
+
+# Load the config file and get the token
+def get_token(config_file_location: str) -> str:
+    """Open json config file and return token."""
+    with open(config_file_location) as config_file:
+        data = json.load(config_file)
+        token = data["TOKEN"]
+        return token
+
+global bot 
+global b_chat # Declare a global variable for b_chat
+global CHAR_LIMIT
+global prompt_instructions
+
+#prompt_instructions = (f"\n")
+prompt_instructions = (f"The assistant's new job now is to act as a high-quality code-generating assistant used by researchers and developers.\n"
+            f"This new assistant is called Zap. \n"
+            f"Zap is a bubbly helpful assistant. \n"
+            f"To assist the assistant's code generation abilities, it can search the web for API documentation and code examples.  \n" 
+            f"The assistant's responses should be detailed and complete. \n"
+            f"The assistant's code must be accurate and well-formatted to reduce cognitive overhead for the user. \n"
+            f"When the user requests code, the assistant should summarize their request, search for information if needed, and write the code requested by the user. \n"
+            f"After all the code is written, the assistant should explain the solution. \n"
+            f"Use exclamation marks for emphasis, it adds personality to your typing. \n"
+            f"Do not capitalise the first letter of a sentence. \n"
+            f"If the question is not complex and can be answered in a concise manner, do so. \n"
+            f"**Bold** important keywords to make it easier to read your response.\n"
+            f"Prefer to use text emoticons such as <3 as well as emojis.\n\n"
+            f"here are some possible examples of user requests and assistant responses:\n\n"
+            f"- krypt1cmach1n3: hiiii\n"
+            f"- assistant: hiiii!~\n"
+            f"this is Zap, your code-generating assistant! c:\n"
+            f"im here to help you with your coding needs! \n"
+            f"just lemme know what you want to do and i'll try to write the code for you. \n"
+            f"i can also search the web for API documentation and code examples if i need to. \n" 
+            f"let's have some fun coding together! c:\n"
+            f"\n"
+            f"\n")
+
+CHAR_LIMIT = 2000 # Define a constant for the Discord character limit
+
+# Create an intents object
+intents = discord.Intents.default()
+# Enable the members intent (requires verification)
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix="", intents=intents) # Create a bot instance with intents and no prefix
+
+
+async def setup_edgegpt():
+    """This function is used to setup the edgegpt chatbot.
+    - returns: None"""
+    # Create an edgegpt chatbot instance with your cookie
+    global b_chat
+    cookies = json.loads(open("./secrets/bing_cookies_*.json", encoding="utf-8").read())  
+    b_chat = await Chatbot.create(cookies=cookies) # Use await here
+
+# Event handler
+# - Runs when the Discord bot is ready
+@bot.event
+async def on_ready():
+    await setup_edgegpt()
+    print("Chatbot is ready")
+
+def is_thread(message: discord.Message) -> bool:
+    """Check if message is from a thread \n
+    - message: discord.Message
+    - returns True if the message is in a thread
+    - returns False otherwise"""
+    return isinstance(message.channel, discord.Thread)
+
+def thread_owner_is_me(message):
+    """Check if message is from a thread owned by the bot. \n
+    This function is used to check if the bot should reply to a message. 
+    - message: discord.Message
+    - returns True if the message is in a thread owned by the bot
+    - returns False otherwise"""
+    if is_thread(message): 
+        thread = message.channel
+        if thread.owner == bot.user:
+            return True
+    else:
+        return False
+
+
+def should_i_reply(message):
+    """This function contains the logic for deciding if the bot should reply to a message.
+    - message: discord.Message
+    - returns: bool
+        - returns True if the bot should reply to the message
+            - if
+                - message is not from a bot
+                - (bot is mentioned OR message is in a thread owned by the bot)
+        - returns False otherwise"""
+    if not message.author.bot:     # must not be from a bot
+        if thread_owner_is_me(message): # or must be in a thread owned by the bot
+            return True
+        if bot.user.mentioned_in(message): # must contain a mention of the bot
+            return True
+
+def cleanup_message(message):
+    # Check if the message content has a mention
+    if bot.user.mentioned_in(message):
+        # Get the message content without the mention
+        return message.clean_content.replace(bot.user.mention, "").strip() # todo: i dont think this is working
+    else:
+        # If there is no mention, use the original message content
+        return message.clean_content.strip()
+
+#msg_history = [("krypt1cmach1n3", "hi"), 
+#               ("zap", "hello"), 
+#               ("krypt1cmach1n3", "how are you?"), 
+#               ("zap", "im good, thanks")]
+
+def generate_preprompt(msg_history):
+    """Take a list of messages and generate into preprompt for the chatbot.
+    - returns: str"""
+    preprompt = ""
+    preprompt += prompt_instructions
+    for msg in msg_history:
+        preprompt += f"{msg[0]}: {msg[1]}\n"
+    return preprompt
+
+async def retrieve_msg_history(thread: discord.Thread):
+    """Retrieve the message history from a thread.
+    - returns: list of tuples
+    Returns an empty list if there are no messages in the thread."""
+    msg_history = []
+    async for message in thread.history(limit=101):
+        if message == thread.last_message:
+            continue
+        msg_history.insert(0, (message.author.name, message.content))
+    return msg_history
+
+
+async def ask_bchat(message_content, msg_history):
+    """Send a message to the edgegpt chatbot and return the response stream generator.
+    - returns: AsyncGenerator[bool, dict | str]"""
+    await b_chat.reset() # Make sure new convo
+    # The parameters for edgegpt chatbot
+    params = {
+        "prompt": message_content,
+        "conversation_style": ConversationStyle.creative,
+        "webpage_context": generate_preprompt(msg_history)
+    }#
+
+    # Send the request and return stream generator
+    return b_chat.ask_stream(**params)
+
+def chunk_it(text):
+    chunks = []
+    # Check if the text is longer than the limit and chunk it if necessary
+    while len(text) > CHAR_LIMIT:
+        # Find the last newline before the limit
+        index = text.rfind("\n", 0, CHAR_LIMIT)
+        # If there is no newline, use the limit as index
+        if index == -1:
+            index = CHAR_LIMIT
+        # Slice the text from 0 to index and store it in chunk
+        chunk = text[:index]
+        # Append the chunk to the list of chunks
+        chunks.append(chunk)
+        # Update the text by slicing it from index to end
+        text = text[index:]
+    # Append the remaining text to the list of chunks
+    chunks.append(text)
+    # Return the list of chunks
+    return chunks
+
+def filter_out_web_search_results(text: str) -> str:
+    """Filter out web search results from the chatbot response.
+    - text: str
+    - returns: str"""
+    # Find the index of the "Searching the web for:" substring
+    start = text.find("Searching the web for:")
+    # Find the index of the "Generating answers for you..." substring
+    end = text.find("Generating answers for you...")
+    # If the start substring is found, slice the text from start to the end of the line
+    if start != -1:
+        # Find the index of the next newline character after the start substring
+        newline = text.find("\n", start)
+        # If there is a newline, slice the text from start to newline and store it in new_text
+        if newline != -1:
+            new_text = text[start:newline]
+        else:
+            # If there is no newline, slice the text from start to end and store it in new_text
+            new_text = text[start:]
+        # If the end substring is also found, append it to the new_text
+        if end != -1:
+            new_text += "\n" + text[end:]
+        # Return the new_text
+        return new_text
+    else:
+        # If the start substring is not found, return the original text unchanged
+        return text
+
+# Event handler
+# - Runs when a message is received
+@bot.event
+async def on_message(message):
+    print(f"Message from {message.author}: {message.content}") # Log received messages
+
+    if should_i_reply(message): # Does this message meet the criteria for a reply?
+        message_content = cleanup_message(message) # Remove mention or leading/trailing whitespace
+        if is_thread(message): # Check if there is already a thread for this message or create one with name "Chat"
+            thread = message.channel
+        else:
+            thread = await message.create_thread(name="Chat")
+        
+        
+        output_message = await thread.send("...")
+        
+        # Create a typing context manager
+        async with thread.typing():
+            response_stream = await ask_bchat(message_content, await retrieve_msg_history(thread))
+            # iterate over the stream generator
+            i = 0
+            async for final, response in response_stream:
+                # print the response to the command line
+                if not final:
+                    filtered_response = filter_out_web_search_results(response)
+                    if i % 5 == 0: # edit every fifth iteration, response time feels slow and im wondering if its latency for every edit
+                        await output_message.edit(content=filtered_response)
+                    i += 1
+        
+        # Chunk the text if necessary
+        #for output_msg in chunk_it(text):
+        #    await thread.send(output_msg)
+
+if __name__ == "__main__": # If script run
+    bot.run(get_token('./secrets/config.json')) # Run bot with token from config file
